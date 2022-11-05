e363d2e16170490baa7346a91f117777124f80b5
Francisco Ramirez <frandixram@gmail.com>
2022-11-05 10:48:42 -0500

    modificaciones para ios

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE = 'static-v3';
+const STATIC_CACHE = 'static-v2.1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
